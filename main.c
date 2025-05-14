037c785ca113976b75d31051abf5199efc79f278
otavig <otaviogarcia.santos4@gmail.com>
2025-05-14 02:56:16 -0300

    feet: Add Base + Interface for navigation

--- main.c
+++ main.c
@@ -2,7 +2,112 @@
 #include <string.h>
 #include <ctype.h>
 
+#define TF 255
+// #define TFP 255
+// #define TFC 255
+// #define TFS 255
+
 int main(void) {
+    /*
+        Formula int for float -> cents / 100.0
+        Formula float for int -> (int)(real * 100 + 0.5)
+
+        Example:
+            float real = 19.99;
+            int conversionNext = (int)(real * 100 + 0.5); 
+            float conversionBack = conversionNext / 100.0; -> (12.3)
+            (Result: 19.99)        
+    */
+
+    // Position at a given physical size
+    int TLV=0, TLP=0, TLC=0, TLS=0;
+
+    // Product
+    char vCod[TF][50], vProdServ[TF][50];
+    int vQtde[TF], vPreco[TF];
+
+    // Client
+    char vRG[TF][11], vCliente[TF][30];
+
+    // Services
+    char vRGV[TF][11], vCodV[TF][50];
+    int vQtdeV[TF], vPrecoV[TF];
+
+    // controllers
+    // Program
+    int program_exit = 1, program_execute_action;
+    char program_confirm_exit;
+
+    // Interface
+    do {
+        printf("\n"
+        "1: Cadastro\n"
+        "2: Excluir\n"
+        "3: Venda\n"
+        "4: Relatórios\n"
+        "5: Finalizar\n\n"
+        "> ");
+
+        scanf("%d", &program_exit);
+
+        switch(program_exit) {
+            case 1:
+                printf("\n"
+                "1: Produtos/Serviços\n"
+                "2: Clientes\n"
+                "3: Retornar\n"
+                "> ");
+                scanf(" %d", &program_execute_action);
+
+                if(program_execute_action == 3) break;
+
+                switch(program_execute_action) {
+                    case 1: 
+                        // Code Products and Services (Otávio && Breno Henrique)
+                        printf("\n Produtos e Serviços selecionado");
+                        break;
+                    case 2:
+                        // Code Client (Rafael && Matheus Bispo)
+                        printf("\n Cliente selecionado");
+                        break;
+                    default:
+                        printf("\nOpção inválida! Tente novamente.\n");
+                        break;
+                }
+                printf("\n");
+                break;
+            case 2: 
+                // Code Delete (Breno Passarela && Kaua)
+                printf("\n Exclusão selecionado \n");
+                break;
+            case 3:
+                // Code Sell (Lucas && Gui Leal)
+                printf("\n Venda selecionado \n");
+                break;
+            case 4: 
+                // Code Report (Otávio && Breno Passarela)
+                printf("\n Relatório selecionado \n");
+                break;
+            case 5: 
+                // Confirm program exit
+                int conditional;
+                do {
+                    printf("Tem certeza ? (S/n) \n");
+                    scanf(" %c", &program_confirm_exit);
+                    // Condition to be checked, it kind of returns 0 for false and 1 for true
+                    conditional = tolower(program_confirm_exit) != 'n' && tolower(program_confirm_exit) != 's';
+                    if(conditional) printf("Resposta inválida! Tente novamente...");
+                } while(conditional);
+
+                if(tolower(program_confirm_exit) == 's') {
+                    program_exit = 0;
+                } else {
+                    program_exit = -1;
+                }
+
+                break;
+        }
+    } while(program_exit!=0);
 
     return 0;
 }
--- readme.md
+++ readme.md
@@ -1,1 +1,10 @@
 # Project CST ADS School
+
+> The code is filled with comments for easy and quick navigation by the team.
+
+## Why choose String for fields like RG and vCods?
+Because we follow a simple rule: when we deal with immutable numbers — meaning numbers we won’t perform arithmetic operations on — they don’t need to be stored as numeric types. Instead, they should be treated as defined text. Additionally, there's a risk of losing the leading zero in an RG (Brazilian ID number), as RGs can start with 0, and storing them as integers may lead to rounding or truncation.
+
+## Why use int for monetary values?
+Although it might seem unnecessary at first glance, using int is actually one of the best industry practices. float or double types can lead to inconsistencies due to precision errors, which may cause small but impactful financial losses. That’s why we usually use decimal or even int. Yes, int — as long as we know where the decimal point is. This way, we can represent cents without losing precision or facing unnecessary rounding.
+Why this choice? Simply due to knowledge and best practices.
